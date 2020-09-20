67e2002a9377fbce7306021e305a298cddf9aabb
Teodora Radeva <52508131+TeodoraRadeva@users.noreply.github.com>
2020-09-20 23:28:02 +0300

    Add files via upload

---
+++ recipes/list/recipes.js
@@ -0,0 +1,44 @@
+let recipes = [
+    {
+        "name": "Синьо мляко с боровинки",
+        "ingredients": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt laborum, minima, qui officia magni, a cum soluta natus placeat atque quidem tempore maiores veritatis, earum fugit et neque minus. Non.",
+        "steps" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptatem perferendis hic, atque aliquam nulla quaerat natus. Mollitia dolorum saepe vitae hic temporibus quis voluptatem voluptatum adipisci, aliquid excepturi consequatur.",
+        "imageURL": "images/bc.jpg",
+        "type": "Десерти"
+    }, 
+    {
+        "name": "Рататуй с Чедър със сорбе от лук",
+        "ingredients": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt laborum, minima, qui officia magni, a cum soluta natus placeat atque quidem tempore maiores veritatis, earum fugit et neque minus. Non.",
+        "steps" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptatem perferendis hic, atque aliquam nulla quaerat natus. Mollitia dolorum saepe vitae hic temporibus quis voluptatem voluptatum adipisci, aliquid excepturi consequatur.",
+        "imageURL" : "images/pc.jpg",
+        "type": "Безмесни"
+    },
+    {
+        "name": "Тиквички-патладжан бюрек",
+        "ingredients": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt laborum, minima, qui officia magni, a cum soluta natus placeat atque quidem tempore maiores veritatis, earum fugit et neque minus. Non.",
+        "steps" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptatem perferendis hic, atque aliquam nulla quaerat natus. Mollitia dolorum saepe vitae hic temporibus quis voluptatem voluptatum adipisci, aliquid excepturi consequatur.",
+        "imageURL": "images/pm.jpg",
+        "type": "Предястия"
+    },
+    {
+        "name": "Табуле с булгур и магданоз",
+        "ingredients": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt laborum, minima, qui officia magni, a cum soluta natus placeat atque quidem tempore maiores veritatis, earum fugit et neque minus. Non.",
+        "steps" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptatem perferendis hic, atque aliquam nulla quaerat natus. Mollitia dolorum saepe vitae hic temporibus quis voluptatem voluptatum adipisci, aliquid excepturi consequatur.",
+        "imageURL": "images/sy.jpg",
+        "type": "Салати"
+    },
+    {
+        "name": "Хляб с маслини",
+        "ingredients": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt laborum, minima, qui officia magni, a cum soluta natus placeat atque quidem tempore maiores veritatis, earum fugit et neque minus. Non.",
+        "steps" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptatem perferendis hic, atque aliquam nulla quaerat natus. Mollitia dolorum saepe vitae hic temporibus quis voluptatem voluptatum adipisci, aliquid excepturi consequatur.",
+        "imageURL": "images/hlqb.jpg",
+        "type": "Тестени"
+    },
+    {
+        "name": "Телешко месе със спанак, гъби и ориз",
+        "ingredients": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt laborum, minima, qui officia magni, a cum soluta natus placeat atque quidem tempore maiores veritatis, earum fugit et neque minus. Non.",
+        "steps" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptatem perferendis hic, atque aliquam nulla quaerat natus. Mollitia dolorum saepe vitae hic temporibus quis voluptatem voluptatum adipisci, aliquid excepturi consequatur.",
+        "imageURL": "images/teleshko.jpg",
+        "type": "Основни"
+    }
+]
